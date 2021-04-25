5225b001a5adb10c85acf6d3e124254574ee13b6
vanesa gonzalez <gvane1919@gmail.com>
2021-04-24 23:08:51 -0400

    re did the api routes to add aggregates to the GET, and finally got it to display on dash board

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,45 +1,62 @@
-const router= require("express").Router();
-const db=require("../models");
+const router = require("express").Router();
+const db = require("../models");
 
 //CREATING THE CRUD FOR THE WORKOUTS W/ AGGREGATION
-router.get("/api/workouts",function(req,res){
-//    db.Workout.find().then(function(results){
-//        res.json(results)
-db.Workout.aggregate([
-    {
-        $addFields:{
-            total
+router.get("/api/workouts", function (req, res) {
+    //    db.Workout.find().then(function(results){
+    //        res.json(results)
+    db.Workout.aggregate([
+        { $addFields: { totalDuration: { $sum: "$exercise.duration" } } }
+    ])
+        .then(function (dbWorkout) { res.json(dbWorkout); })
 
-        }
-    }
-])
-
-
-
-   })
-})
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+});
 
 
+router.put("/api/workouts/:id", function (req, res) {
+    db.Workout.findByIdAndUpdate({ _id: req.params.id },
+        { $push: { exercises: req.body }, $inc: { totalDuration: req.body.totalDuration } })
+        .then(function (dbWorkout) {
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+});
 
-
-
-router.put("/api/workouts/:id",function(req,res){
-    db.Workout.findByIdAndUpdate({_id:req.params.id},{$push:{exercises:req.body}}).then(function(results){
-        res.json(results)
+router.post("/api/workouts", function (req, res) {
+    db.Workout.create(req.body).then(function (dbWorkout) {
+        res.json(dbWorkout)
     })
-})
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+});
 
-router.post("/api/workouts",function(req,res){
-    db.Workout.create(req.body).then(function(results){
-        res.json(results)
-    })
-})
+//this is what you see in the dashboard total duration and pounds lifted
 
+router.get("/api/workouts/range", function (req, res) {
+    // db.Workout.find().then(function (results) {
+    //     res.json(results)
+    // })
+    db.Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: '$exercises.duration' },
+                totalWeight: { $sum: '$exercises.weight' },
+            },
+        },
+    ])
+        .limit(7)
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+});
 
-router.get("/api/workouts/range",function(req,res){
-    db.Workout.find().then(function(results){
-        res.json(results)
-    })
- })
-
- module.exports= router
+module.exports = router
